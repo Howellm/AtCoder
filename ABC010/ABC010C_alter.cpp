fc3e83214b419f6a383356198952f4b06a209fd6
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-04 23:10:42 +0900

    WA

---
+++ ABC010/ABC010C_alter.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int distance(int X, int Y, int tx, int ty){
+    int root = (X - tx) * (X - tx) + (Y - ty) * (Y - ty);
+    return sqrt(root);
+}
+
+int main(){
+    int tx_a, ty_a, tx_b, ty_b, T, V, n;
+    bool flag = false;
+    cin >> tx_a >> ty_a >> tx_b >> ty_b >> T >> V >> n;
+    for(int i = 0; i < n; i ++){
+        int x, y;
+        cin >> x >> y;
+        if(distance(tx_a, ty_a, x, y) + distance(x, y, tx_b, ty_b) <= T * V){
+            flag = true;
+            break;
+        }
+    }
+    if(flag){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+}
