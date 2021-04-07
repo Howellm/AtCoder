fa563bd380f517c2ad81f90fa9ba310a043f4d22
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-07 17:06:42 +0900

    WA

---
+++ ABC059/ABC059C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, res_1 = 0, res_2 = 0, sum = 0;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        sum += a[i];
+        if(i%2 == 0 and sum >= 0){
+            res_1 += sum + 1;
+            sum = -1;
+        }else if(i%2 != 0 and sum <= 0){
+            res_1 += abs(sum) + 1;
+            sum = 1;
+        }
+    }
+    sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += a[i];
+        if(i%2 == 0 and sum <= 0){
+            res_2 += abs(sum) + 1;
+            sum = 1;
+        }else if(i%2 != 0 and sum >= 0){
+            res_2 += sum + 1;
+            sum = -1;
+        }
+    }
+    
+    cout << min(res_1,res_2) << endl;
+}
