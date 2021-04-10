e6dce6267622f7ec22943829e139586c7e5e3006
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-10 17:25:59 +0900

    Update

---
+++ ABC/ABC096/ABC096C.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Repain(){
+  
+}
+
+int main(){
+  int H,W;
+  char ch;
+  int grid[H][W];
+  for(int i = 0; i < H; i++){
+    for(int j = 0; j < W; j++){
+      cin >> ch;
+      if(ch == "#"){
+        grid[i][j] = 1;
+      }else}
+        grid[i][j] = 0;
+    }
+
+}
