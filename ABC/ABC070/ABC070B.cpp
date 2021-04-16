f05dd83021d694c5048685ec592f80f93f6388f9
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-16 19:43:36 +0900

    AC

--- ABC/ABC070/ABC070B.cpp
+++ ABC/ABC070/ABC070B.cpp
@@ -4,20 +4,8 @@ using namespace std;
 int main(){
   int A, B, C, D,sum = 0;
   cin >> A >> B >> C >> D;
-  if(A < D and B > C){
-    if(A >= C){
-      if(B > D){
-        sum = D - A;
-      }else{
-        sum = B - A;
-      }
-    }else{
-      if(D > B){
-        sum = B - C;
-      }else{
-        sum = D - C;
-      }
-    }
-  }
+  int end = min(B,D);
+  int start = max(A,C);
+  sum = (end - start > 0)? (end - start) : 0;
   cout << sum << endl;
 }
