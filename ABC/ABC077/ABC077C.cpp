30288c88d03aade6e12828abbac70740ff003afd
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-17 21:38:11 +0900

    AC

--- ABC/ABC077/ABC077C.cpp
+++ ABC/ABC077/ABC077C.cpp
@@ -14,29 +14,28 @@ int binary_search(int key){
   return -1;
 }
 
-int main(){
-  cout << binary_search(1) << endl;
-}
-
 // int main(){
-//   int N;
-//   cin >> N;
-//   vector<vector<int> > snuke(3,vector<int>(N));
-//   for(int i = 0; i < 3; i++){
-//     for(int j = 0; j < N; j++){
-//       cin >> snuke[i][j];
-//     }
-//   }
-//   int cnt = 0;
-//   for(int i = 0; i < N; i++){
-//     for(int j = 0; j < N; j++){
-//       int res_a = snuke[2][i] - snuke[1][j];
-//       if(res_a <= 0) continue;
-//       for(int k = 0; k < N; k++){
-//         int res_b = snuke[1][j] - snuke[0][k];
-//         if(res_b > 0) cnt++;
-//       }
-//     }
-//   }
-//   cout << cnt << endl;
+//   cout << binary_search(1) << endl;
 // }
+
+int main(){
+  int N;
+  cin >> N;
+  vector<long long> a(N), b(N), c(N);
+  for (int i = 0; i < N; ++i) cin >> a[i];
+  for (int i = 0; i < N; ++i) cin >> b[i];
+  for (int i = 0; i < N; ++i) cin >> c[i];
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  sort(c.begin(), c.end());
+
+  long long res = 0;
+  for(int j = 0; j < N; j++){
+    long long Aj = lower_bound(a.begin(), a.end(), b[j]) - a.begin();
+    long long Cj = N - (upper_bound(c.begin(), c.end(), b[j]) - c.begin());
+    res += Aj * Cj;
+  }
+  long long Cj = a.end();
+  cout << Cj << endl;
+}
