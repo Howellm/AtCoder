ca0c2d8538ca3bbbdade2d8011302fa27e9ad8b5
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-19 19:45:36 +0900

    RE

---
+++ ABC/ABC135/ABC135C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  vector<long long> A(N + 5,0), B(N + 5,0);
+  cin >> N;
+  for(int i = 0; i < N + 1; i++) cin >> A[i];
+  for(int i = 0; i < N; i++) cin >> B[i];
+
+  long long sum = 0;
+  for(int i = 0; i < N; i++){
+    if(A[i] > B[i]){
+      sum += B[i];
+    }else{
+      sum += min(B[i], A[i] + A[i + 1]);
+      A[i + 1] = A[i + 1] > (B[i] - A[i]) ? A[i + 1] - (B[i] - A[i]) : 0;
+      // if(A[i+1] >= B[i] - A[i]){
+      //   A[i + 1] -= (B[i] - A[i]);
+      // }else{
+      //   A[i + 1] = 0;
+      // }
+    }
+  }
+  cout << sum << endl;
+  return 0;
+}
